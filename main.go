faca6ef3a680b90ddce13c6aa27c0cf982a8f601
Koji <stylethewalker@gmail.com>
2017-01-12 07:52:26 -0200

    add ui

--- main.go
+++ main.go
@@ -1,13 +1,49 @@
 package main
 
-import
-// "net/http"
-// "github.com/andlabs/ui"
+import (
+	// "net/http"
+	"time"
 
-"github.com/labstack/echo"
+	"github.com/andlabs/ui"
+	"github.com/labstack/echo"
+)
 
 func main() {
 	e := echo.New()
 	e.Static("/", "./")
-	e.Logger.Fatal(e.Start(":8080"))
+
+	uiErr := ui.Main(func() {
+		startBtn := ui.NewButton("Start")
+		stopBtn := ui.NewButton("Stop")
+		greeting := ui.NewLabel("greet")
+		box := ui.NewVerticalBox()
+		window := ui.NewWindow("Quick Serv", 200, 100, false)
+		window.SetChild(box)
+
+		box.Append(greeting, false)
+		box.Append(startBtn, false)
+		box.Append(stopBtn, false)
+
+		// Events
+		startBtn.OnClicked(func(*ui.Button) {
+			e.Logger.Fatal(e.Start(":8080"))
+		})
+
+		stopBtn.OnClicked(func(*ui.Button) {
+			e.Shutdown(1 * time.Second)
+		})
+
+		window.OnClosing(func(*ui.Window) bool {
+			// e.ShutdownTimeout = 1 * time.Second
+			e.Shutdown(1 * time.Second)
+			ui.Quit()
+			return true
+		})
+
+		window.Show()
+	})
+	if uiErr != nil {
+		panic(uiErr)
+	}
+
 }
