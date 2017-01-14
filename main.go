f5b53de22398be914db4591fdcd8c62bdbd11bfd
Koji <stylethewalker@gmail.com>
2017-01-14 13:02:20 -0200

    add: cache-controll to header, fix shutdown bug

--- main.go
+++ main.go
@@ -1,46 +1,56 @@
 package main
 
 import (
-	// "net/http"
-
 	"fmt"
-	"time"
+	"runtime"
 
 	"github.com/andlabs/ui"
-	"github.com/labstack/echo"
+	"github.com/skratchdot/open-golang/open"
+	"github.com/walkerEpps/quick_serve/server"
 )
 
 func main() {
-	e := echo.New()
-	e.Static("/", "./www/")
+	command := "milk"
+	if runtime.GOOS == "windows" {
+		command += ".bat"
+	}
+	fmt.Println(command)
+	// init := false
+
+	server := server.New()
 
 	uiErr := ui.Main(func() {
 		startBtn := ui.NewButton("Start")
 		stopBtn := ui.NewButton("Stop")
-		greeting := ui.NewLabel("greet")
+		openBrowserBtn := ui.NewButton("Open Browser")
+		status := ui.NewLabel("Server is Stoped")
 		box := ui.NewVerticalBox()
 		window := ui.NewWindow("Quick Serv", 200, 100, false)
 		window.SetChild(box)
 
-		box.Append(greeting, false)
+		box.Append(status, false)
 		box.Append(startBtn, false)
 		box.Append(stopBtn, false)
+		box.Append(openBrowserBtn, false)
 
 		// Events
 		startBtn.OnClicked(func(*ui.Button) {
-			fmt.Println("hello")
-			go func() {
-				e.Logger.Fatal(e.Start(":8080"))
-			}()
+			server.Run()
+			status.SetText("Server is Running")
 		})
 
 		stopBtn.OnClicked(func(*ui.Button) {
-			e.Shutdown(1 * time.Second)
+			status.SetText("Server is Stopped")
+			server.Stop()
+		})
+
+		openBrowserBtn.OnClicked(func(*ui.Button) {
+			open.Run("http://localhost:" + server.Port)
 		})
 
 		window.OnClosing(func(*ui.Window) bool {
-			// e.ShutdownTimeout = 1 * time.Second
-			e.Shutdown(1 * time.Second)
+
+			server.Stop()
 			ui.Quit()
 			return true
 		})
@@ -50,5 +60,4 @@ func main() {
 	if uiErr != nil {
 		panic(uiErr)
 	}
-
 }
